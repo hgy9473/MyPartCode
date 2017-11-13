5d5202da714b360a9d23c698a2d05045aac54a13
hgy9473 <hgy9473@foxmail.com>
2017-11-13 15:46:20 +0800

    添加了一些注释 又测试了一下

--- NodeJS/koa/sample01/app.js
+++ NodeJS/koa/sample01/app.js
@@ -9,7 +9,16 @@ const Koa = require('koa');
 const app = new Koa();
 
 // 对于任何请求，app将调用该异步函数处理请求：
-app.use((ctx, next) => {
+// 参数ctx是由koa传入的封装了request和response的变量，我们可以通过它访问request和response
+// next是koa传入的将要处理的下一个异步函数
+var cnt = 0;
+app.use(async (ctx, next) => {
+    console.log("ctx 格式化打印：");
+    console.log(JSON.stringify(ctx,null,'---'));
+    console.log("next 打印：");
+    console.log(next.toString());
+    console.log("请求次数：",++cnt);
+    await next();
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
